032672ce17094d36f4c412f84001f06981dc5e8a
Marvin Rudolph <rudolph.marvin@web.de>
2021-06-15 17:45:34 +0200

    Initial Commit

---
+++ SwiftApolloWebsocket/SwiftApolloWebsocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftApolloWebsocketApp.swift
+//  SwiftApolloWebsocket
+//
+//  Created by Marvin Rudolph on 15.06.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftApolloWebsocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
